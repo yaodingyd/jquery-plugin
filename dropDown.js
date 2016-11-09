270df6256cffa22fd417d6eb9714ab7f0ccb9ed8
Yao Ding <yaodingyd@users.noreply.github.com>
2016-11-08 21:24:34 -0500

    Update dropDown.js

--- dropDown.js
+++ dropDown.js
@@ -3,6 +3,18 @@
  *
  * There could a hidden input, and dropdown item could have a data-value attribute.
  * So after selecting, the input value could also be updated. 
+ * 
+ * Example:
+    <div class="dropdown" >
+        <input type="text" class="hidden-xs-up" id="lang" name="lang" value="<?= $data['lang']; ?>">
+        <button class="btn btn-secondary dropdown-toggle" type="button"  data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+            <span href="#" class="flag-icon <?= $flag; ?>"></span>
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="locale-dropdown">
+            <li class="dropdown-item" data-value="en"><span class="flag-icon flag-icon-gb"></span></li>
+            <li class="dropdown-item" data-value="dk"><span class="flag-icon flag-icon-dk"></span></li>
+        </ul>
+    </div>
  *
  */
 
